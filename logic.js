313be97af8c1a561f2fba4ad004b5cd08f3fe1b7
charcmani <shubhanshu.085@gmail.com>
2018-03-25 12:28:20 +0530

    final changes in word extraction

--- logic.js
+++ logic.js
@@ -1,7 +1,7 @@
 
 function wordFreq(string) {
 	var l=-1,h=-1;
-	var words = string.replace(/[?–,()0-9;""]/g,).split(/\s/);
+	var words = string.replace(/[?–,;()0-9]/g, '').split(/\s/);
 	//Regex to remove unwanted characters and numbers
     //console.log(words);
     for (var i=0;i<words.length;i++)
